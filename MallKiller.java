8b8575b12e4954d603130c600cc562fc0a14b554
shreyas <shreyasgkgowda39@gmail.com>
2023-07-21 08:44:15 +0530

    constructor

---
+++ MallKiller.java
@@ -0,0 +1,18 @@
+class MallKiller{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Invoking in MallKiller");
+		
+		Mall mall = new Mall("Gopalan arcade","Rajarajeswarinagar");
+		
+		System.out.println("==============================");
+		
+		Mall mall1 = new Mall("Gopalan arcade","Rajarajeswarinagar","Shopping mall");
+		
+		System.out.println("=============================");
+		
+		Mall mall2=new Mall("Gopalan arcade","Rajarajeswarinagar","Shopping mall",45,"Deepak",2000.0f,400);
+		
+	}
+}
