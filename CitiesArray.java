4188c9c5d8f3789aa4a846fc116314ad9ba811db
shreyas <shreyasgkgowda39@gmail.com>
2023-07-27 18:03:25 +0530

    contructor

---
+++ CitiesArray.java
@@ -0,0 +1,30 @@
+class CitiesArray{
+	public static void main(String[] args)
+	{
+		System.out.println("Running main in CitiesArray");
+		{
+		String shimoga  = "Shimoga";
+		String chikmangluru = "Chikmangluru";
+		String bangalore = "Bangalore";
+		String tumkuru = "Tumkuru";
+		String udupi = "Udupi";
+		String mysuru = "Mysuru";
+		String hassan = "Hassan";
+		String kolar = "Kolar";
+		
+		String[] names ={shimoga,chikmangluru,bangalore,tumkuru,udupi,mysuru,hassan,kolar};
+		
+		String ref5 = names[5];
+		System.out.println("cities in karnataka index 5 :"+ref5);
+		
+		String ref4 = names[4];
+		System.out.println("cities in karnataka index 4 :"+ref4);
+		
+		String ref1 = names[1];
+		System.out.println("cities in karnataka index 1  :"+ref1);
+		
+		String ref3 = names[3];
+		System.out.println("cities in karnataka index 3  :"+ref3);
+	}
+	}
+}
